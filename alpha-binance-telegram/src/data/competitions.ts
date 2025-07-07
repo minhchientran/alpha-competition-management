a69fb1fe574d80f1ce9ea2c514a42c933843cd6e
cotb <tranbinhco99@gmail.com>
2025-07-07 21:35:22 +0700

    rm avail

--- alpha-binance-telegram/src/data/competitions.ts
+++ alpha-binance-telegram/src/data/competitions.ts
@@ -7,7 +7,6 @@ export interface Competition {
 }
 
 export const competitionsData: Competition[] = [
-    { id: 14, tokenName: 'AVAIL', condition: 'Top 12500', deadline: '04/07/2025 14:00:00', reward: 2600 },
     { id: 16, tokenName: 'BULLA', condition: 'Top 11700', deadline: '06/07/2025 17:00:00', reward: 1000 },
     { id: 17, tokenName: 'CARV', condition: 'Top 8300', deadline: '07/07/2025 14:00:00', reward: 360 },
     { id: 18, tokenName: 'REKT', condition: 'Top 12500', deadline: '12/07/2025 20:00:00', reward: 277800000, },
