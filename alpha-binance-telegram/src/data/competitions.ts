f9bca558e55a154f9f2312bfbe82be9fef4e90e8
Co Tran <tranbinhco99@gmail.com>
2025-07-09 16:15:01 +0700

    add oik

--- alpha-binance-telegram/src/data/competitions.ts
+++ alpha-binance-telegram/src/data/competitions.ts
@@ -7,10 +7,10 @@ export interface Competition {
 }
 
 export const competitionsData: Competition[] = [
-    { id: 16, tokenName: 'BULLA', condition: 'Top 11700', deadline: '06/07/2025 17:00:00', reward: 1000 },
     { id: 17, tokenName: 'CARV', condition: 'Top 8300', deadline: '07/07/2025 14:00:00', reward: 360 },
     { id: 18, tokenName: 'REKT', condition: 'Top 12500', deadline: '12/07/2025 20:00:00', reward: 277800000, },
     { id: 19, tokenName: 'FUEL', condition: 'Top 9000', deadline: '13/07/2025 20:00:00', reward: 11000 },
     { id: 20, tokenName: 'BGSC', condition: 'Top 12500', deadline: '15/07/2025 15:30:00', reward: 10900 },
     { id: 21, tokenName: 'MPLX', condition: 'Top 12500', deadline: '20/07/2025 20:00:00', reward: 713 },
+    { id: 22, tokenName: 'OIK', condition: 'Top 5000', deadline: '22/07/2025 20:00:00', reward: 1000 },
 ]; 
